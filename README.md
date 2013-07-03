5c383f19f6c4558ccc94324dc1f3a46af133010b
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-07-02 20:39:30 -0400

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,46 @@ timerjs
 =======
 
 A lightweight javascript timer
+
+# usage
+
+## examples
+
+Create a timer object by passing it at least the seconds option, in this case with 180 seconds
+
+    var timer = new Timer({
+        seconds: 180,
+    });
+    timer.play();
+
+The toString() method returns the time in mm:dd format
+
+    console.log(timer.toString());
+    >> 02:03
+
+You can set a callback for when the time runs out
+
+    var timer = new Timer({
+        seconds: 180,
+        onTimeout: function () {
+            alert('timeout!');
+        }
+    });
+
+You can set a callback for each tick of the clock
+
+    var timer = new Timer({
+        seconds: 180,
+        onTimeout: function () {
+            alert('timeout!');
+        }
+        onTick: function (timer) {
+            console.log(timer.toString());
+        }
+    });
+
+## options
+
+* seconds: the number of seconds until the time runs out
+* onTimeout: a callback for when the time runs out
+* onTick: a callback for each tick of the clock
---
+++ timer.js
@@ -0,0 +1,81 @@
+/*global clearInterval: false */
+/*global setInterval: false */
+
+var Timer = function (options) {
+    "use strict";
+
+    this.PAUSED = 1;
+    this.PLAYING = 2;
+    this.ENDED = 3;
+
+    this.restart = function () {
+        this.secondsLeft = options.seconds || 0;
+	    this.millisecondsLeft = this.secondsLeft * 1000;
+    };
+
+	this.play = function () {
+        var self = this;
+        if (!this.state) {
+	        this.state = this.PAUSED;
+            this.onTimeout = options.onTimeout || function () {};
+            this.onTick = options.onTick || function () {};
+            this.restart();
+        }
+
+        if (this.state === this.PAUSED) {
+			this.timeMark = Date.now();
+			this.interval = setInterval(
+                function () {
+                    self.update();
+                },
+                50
+            );
+            this.state = this.PLAYING;
+		}
+	};
+
+	this.update = function () {
+		if (this.millisecondsLeft <= 0) {
+		    clearInterval(this.interval);
+            this.state = this.ENDED;
+            this.onTimeout();
+            return;
+		}
+
+		var currentTime = Date.now(), seconds;
+
+		this.millisecondsLeft -= currentTime - this.timeMark;
+		this.timeMark = currentTime;
+
+        seconds = this.millisecondsLeft / 1000;
+		if (seconds < this.secondsLeft) {
+            console.log(this.millisecondsLeft);
+            this.secondsLeft = parseInt(seconds, 10);
+            this.onTick(this);
+        }
+	};
+
+	this.pause = function () {
+        this.state = this.PAUSED;
+        this.pauseTime = new Date();
+		clearInterval(this.interval);
+	};
+
+    this.toString = function () {
+        var minutes, seconds, secondsLeft;
+        if (this.millisecondsLeft > 0) {
+            secondsLeft = parseInt(this.millisecondsLeft / 1000, 10) + 1;
+        } else {
+            secondsLeft = 0;
+        }
+
+        minutes = parseInt(secondsLeft / 60, 10);
+
+        seconds = secondsLeft % 60;
+
+        minutes = ((minutes.toString()).length === 2 ? '' : '0') + minutes;
+        seconds = ((seconds.toString()).length === 2 ? '' : '0') + seconds;
+
+        return minutes + ':' + seconds;
+    };
+};
